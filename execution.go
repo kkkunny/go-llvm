9cb591c621a34254b50b6936951ae638ec575603
kkkunny <1401289859@qq.com>
2023-12-11 13:01:39 +0800

    增加获取运行时地址

--- execution.go
+++ execution.go
@@ -2,6 +2,7 @@ package llvm
 
 import "C"
 import (
+	"os"
 	"unsafe"
 
 	"github.com/samber/lo"
@@ -35,39 +36,41 @@ func (v ExecutionValue) Float(t FloatType) float64 {
 	return binding.LLVMGenericValueToFloat(t.binding(), v.binding())
 }
 
-type ExecutionEngine struct{
-	bind binding.LLVMExecutionEngineRef
-	
+type ExecutionEngine struct {
+	bind   binding.LLVMExecutionEngineRef
+	module Module
+
 	funcMaps map[uint64]any
 }
 
-func newExecutionEngine(b binding.LLVMExecutionEngineRef)*ExecutionEngine{
+func newExecutionEngine(m Module, b binding.LLVMExecutionEngineRef) *ExecutionEngine {
 	return &ExecutionEngine{
-		bind: b,
+		bind:     b,
+		module:   m,
 		funcMaps: make(map[uint64]any),
 	}
 }
 
 func NewExecutionEngine(m Module) (*ExecutionEngine, error) {
 	b, err := binding.LLVMCreateExecutionEngineForModule(m.binding())
-	return newExecutionEngine(b), err
+	return newExecutionEngine(m, b), err
 }
 
 func NewInterpreter(m Module) (*ExecutionEngine, error) {
 	b, err := binding.LLVMCreateInterpreterForModule(m.binding())
-	return newExecutionEngine(b), err
+	return newExecutionEngine(m, b), err
 }
 
 func NewJITCompiler(m Module, opt CodeOptLevel) (*ExecutionEngine, error) {
 	b, err := binding.LLVMCreateJITCompilerForModule(m.binding(), uint32(opt))
-	return newExecutionEngine(b), err
+	return newExecutionEngine(m, b), err
 }
 
 func DefaultMCJITCompiler(m Module) (*ExecutionEngine, error) {
 	var option binding.LLVMMCJITCompilerOptions
 	binding.LLVMInitializeMCJITCompilerOptions(&option)
 	b, err := binding.LLVMCreateMCJITCompilerForModule(m.binding(), option)
-	return newExecutionEngine(b), err
+	return newExecutionEngine(m, b), err
 }
 
 func (engine ExecutionEngine) binding() binding.LLVMExecutionEngineRef {
@@ -82,6 +85,10 @@ func (engine ExecutionEngine) RunMainFunction(f Function, argv, envp []string) u
 	return uint8(binding.LLVMRunFunctionAsMain(engine.binding(), f.binding(), argv, envp))
 }
 
+func (engine ExecutionEngine) RunMainFunctionWithParentEnv(f Function) uint8 {
+	return engine.RunMainFunction(f, os.Args, os.Environ())
+}
+
 func (engine ExecutionEngine) RunFunction(f Function, args ...ExecutionValue) ExecutionValue {
 	as := lo.Map(args, func(item ExecutionValue, index int) binding.LLVMGenericValueRef {
 		return item.binding()
@@ -89,13 +96,26 @@ func (engine ExecutionEngine) RunFunction(f Function, args ...ExecutionValue) Ex
 	return ExecutionValue(binding.LLVMRunFunction(engine.binding(), f.binding(), as))
 }
 
-func (engine ExecutionEngine) GetFunction(name string) *Function {
+func (engine ExecutionEngine) GetFunction(name string) (Function, bool) {
 	v, fail := binding.LLVMFindFunction(engine.binding(), name)
-	if fail {
-		return nil
+	if !fail {
+		return Function(v), !v.IsNil()
 	}
-	f := Function(v)
-	return &f
+	return engine.module.GetFunction(name)
+}
+
+func (engine ExecutionEngine) GetFunctionRuntimePointer(f Function) (unsafe.Pointer, bool) {
+	res := binding.LLVMGetPointerToGlobal(engine.bind, f.binding())
+	return res, res != nil
+}
+
+func (engine ExecutionEngine) GetVariable(name string) (GlobalValue, bool) {
+	return engine.module.GetGlobal(name)
+}
+
+func (engine ExecutionEngine) GetVariableRuntimePointer(v GlobalValue) (unsafe.Pointer, bool) {
+	res := binding.LLVMGetPointerToGlobal(engine.bind, v.binding())
+	return res, res != nil
 }
 
 // MapGlobalToC 映射全局值到c语言值
