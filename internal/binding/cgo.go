73b2d58048e2487ee9474892ca8cb9f413b69068
kkkunny <1401289859@qq.com>
2026-09-22 17:10:09 +0800

    feat: 内置cgo编译链接flags并适配LLVM22

---
+++ internal/binding/cgo.go
@@ -0,0 +1,9 @@
+package binding
+
+/*
+#cgo CFLAGS: -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS
+#cgo CFLAGS: -I/usr/lib/llvm-22/include -I/usr/include -I/usr/local/include
+#cgo CXXFLAGS: -std=c++17 -D_GNU_SOURCE -D_GLIBCXX_USE_CXX11_ABI=1 -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS
+#cgo LDFLAGS: -L/usr/lib/llvm-22/lib -L/usr/lib64 -L/usr/lib -L/usr/local/lib -lLLVM
+*/
+import "C"
